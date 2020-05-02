0e2b953d53241e103e6c833a42a1f0b5b90203ab
Nicolas Carranza <nicarran@gmail.com>
2020-05-02 04:02:53 -0500

    Fix lineMap position mismatch
    LineMap.getPosition(line,col) expands tabs to spaces causing position mismatch when editing source
    files with tabs, use getStartPosition instead.

--- src/main/java/org/javacomp/parser/LineMapUtil.java
+++ src/main/java/org/javacomp/parser/LineMapUtil.java
@@ -7,7 +7,7 @@ public final class LineMapUtil {
   private LineMapUtil() {}
 
   public static int getPositionFromZeroBasedLineAndColumn(LineMap lineMap, int line, int column) {
-    // LineMap accepts 1-based line and column numbers.
-    return (int) lineMap.getPosition(line + 1, column + 1);
+    // LineMap accepts 1-based line.
+    return (int)lineMap.getStartPosition(line+1)+column;
   }
 }
